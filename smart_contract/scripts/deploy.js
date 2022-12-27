e4cd420c387074e77045642d2977d4e4c66ff18a
abhishekbagade1 <abhishekbagade8@gmail.com>
2022-12-27 23:29:38 +0530

    new files added

---
+++ smart_contract/scripts/deploy.js
@@ -0,0 +1,18 @@
+const {ethers} = require("hardhat");
+
+async function main() {
+
+  const whitelistContract = await ethers.getContractFactory("Whitelist");
+  const deployedWhitelistContract = await whitelistContract.deploy(10);
+  // 10 is the Maximum number of whitelisted addresses allowed
+  await deployedWhitelistContract.deployed();
+
+  console.log("Whitelist Contract Address:", deployedWhitelistContract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  })
